6dbf6982c48ec9221abf2b9bfeb8df42f07d94f0
Yan Li <eyeplum@gmail.com>
2016-12-15 11:48:30 +0800

    Remove unused code

--- Sources/Utilities.swift
+++
@@ -1,3 +0,0 @@
-internal func Unimplemented(_ fn: String = #function, file: StaticString = #file, line: UInt = #line) -> Never {
-    fatalError("\(fn) is not yet implemented", file: file, line: line)
-}
